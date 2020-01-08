d382cb9453e45542eb3cd293a4565b82a1fd3e15
schm4131 <schm4131@umn.edu>
2020-01-08 17:19:17 -0600

    Took advantage of airity overloading on fib_max_n, other functions remain unchanged from first commit

--- src/euler_2/core.clj
+++ src/euler_2/core.clj
@@ -9,6 +9,7 @@
 
 (defn fib [n]
   ;;* fib returns the nth Fibonacci number.
+
   (cond
     (or (= n 2) (= n 1)) n
     :else (+ (fib (- n 2)) (fib (- n 1)))
@@ -18,13 +19,18 @@
 (defn fib_max_n [fib_max n]
   ;;* fib_max_n returns the value of n such that the (n + 1)th Fibonacci number
   ;;* is greater than fib_max but the nth Fibonacci number is not.
-  
-  ;;? I'm pretty sure I can use arity overloading to provide a default value to n,
-  ;;? but I am unsure of how to best do this.
 
-  (cond
-    (and (< (fib n) fib_max) (not (< (fib (+ n 1)) fib_max))) n
-    :else (fib_max_n fib_max (+ n 1))
+  ([fib_max]
+    (cond
+      (and (< (fib 1) fib_max) (not (< (fib 2) fib_max))) n
+      :else (fib_max_n fib_max 2)
+    )
+  )
+  ([fib_max n]
+    (cond
+      (and (< (fib n) fib_max) (not (< (fib (+ n 1)) fib_max))) n
+      :else (fib_max_n fib_max (+ n 1))
+    )
   )
 )
 
@@ -42,7 +48,7 @@
 (defn sum_even_fib [fib_max]
   ;;* sum_even_fib adds all even Fibonacci numbers that are lower than fib_max.
 
-  (def fib_vector (fib_vectorizer (fib_max_n fib_max 1)))
+  (def fib_vector (fib_vectorizer (fib_max_n fib_max)))
   (reduce + ( filter #(= (mod % 2) 0) fib_vector))
 )
 
